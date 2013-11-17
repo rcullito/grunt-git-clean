a73e69602bc5e4d03f8c269b25fdd6067f3ca147
Rob Culliton <rculliton@yieldbot.com>
2013-11-17 11:28:44 -0500

    better error handling for both git uninitialized & when there are no files to remove

--- tasks/git_clean.js
+++ tasks/git_clean.js
@@ -7,8 +7,9 @@
  */
 
 
- var exec = require('child_process').exec;
-var readline = require('readline');
+var exec = require('child_process').exec,
+  util = require('util'),
+  readline = require('readline');
 
 var rl = readline.createInterface({
   input: process.stdin,
@@ -16,31 +17,41 @@ var rl = readline.createInterface({
 });
 
 
-
-
 'use strict';
 
 module.exports = function(grunt) {
 
   grunt.registerMultiTask('git_clean', 'git clean directories of your choice', function() {
-
+    // first verify that it is a git repository and verify if this ins't the case
     var done = this.async();
-    exec('git clean -n', function(error, stdout, stderr) {
-      console.log(stdout);
-
-      rl.question("Proceed with removing the above files? (y/n)", function(answer) {
-        if (answer === "y") {
-          exec('git clean -f', function(error, stdout, stderr) {
-            console.log(stdout);
-            rl.close();
+    exec('git status', function(error, stdout, stderr) {
+      if (error && error.code === 128) {
+        grunt.fail.fatal(stderr);
+        done();
+      } else {
+        exec('git clean -n', function(error, stdout, stderr) {
+          grunt.log.writeln(stdout);
+          // only execute this logic if there are files that have been modified
+          if (!stdout) {
+            grunt.log.writeln('Git found no files to remove');
             done();
-          });
-        } else {
-          console.log("Files will be kept in working directory.");
-          rl.close();
-          done();
-        }
-      });
+          } else {
+            rl.question("Proceed with removing the above files? (y/n)", function(answer) {
+              if (answer === "y") {
+                exec('git clean -f', function(error, stdout, stderr) {
+                  grunt.log.writeln(stdout);
+                  rl.close();
+                  done();
+                });
+              } else {
+                grunt.log.writeln("Files will be kept in working directory.");
+                rl.close();
+                done();
+              }
+            });
+          }
+        });
+      }
     });
   });
 };
